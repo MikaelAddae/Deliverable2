ce4076492003fd19643923916146d0692bb6be9f
MikaelAddae <129565826+MikaelAddae@users.noreply.github.com>
2023-07-10 12:51:14 -0400

    AddCommentsAndRevisit

--- Deliverable2/Program.cs
+++ Deliverable2/Program.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 
 namespace Deliverable2
+    //pre course deliverable
 {
     public class Program
     {
@@ -8,23 +9,35 @@ namespace Deliverable2
         {
             Console.WriteLine("Welcome to the coin flip challenge!");
             Console.WriteLine("What is your name?");
+
+            //capture name as a string variable 
             string name = Console.ReadLine();
             Console.WriteLine("Welcome " + name + " Would you like to do the coin flip challenge? (Yes/No))");
+
+            //capture answer as a string variable, make all characters lower case
             string answer = Console.ReadLine().ToLower();
+
+            //compares answer to pre-defined values
             if (answer == "no")
             {
                 Console.WriteLine("You are a coward " + name);
             }
-            if (answer == "yes") {
+            if (answer == "yes")
+            {
+                //variables for number of correct guesses, and number of flips
                 int count = 0;
                 int correct = 0;
 
+                //C# random number variable
                 Random rnd = new Random();
+
+                //loop that repeats until 5 flips have occurred
                 do
                 {
                     Console.WriteLine("Heads or Tails?");
                     string choice = Console.ReadLine().ToLower();
                     bool flip = (choice == "heads");
+                    //random number between 0(inclusive) and 2(exclusive), assigned 0 to heads, and 1 to tails
                     int result = rnd.Next(0, 2);
                     if (result == 1)
                     {
@@ -43,7 +56,7 @@ namespace Deliverable2
                     {
                         Console.WriteLine("Heads!");
                         count = count + 1;
-                        
+
                         {
                             if (flip == true && choice == "heads")
                             {
@@ -54,9 +67,10 @@ namespace Deliverable2
                     }
                 }
                 while (count < 5);
-                    { Console.WriteLine("Thank you " + name + "! You were correct (" + correct + "/5).");
-                    }
-             } 
+                {
+                    Console.WriteLine("Thank you " + name + "! You were correct (" + correct + "/5).");
+                }
+            }
         }
     }
 }
